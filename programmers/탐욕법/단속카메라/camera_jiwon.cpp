8f58cc3b71e5ad0159f777e0cd770324ee489b5b
Hanturtle <tjfdnjs0829@naver.com>
2021-02-17 18:26:14 +0900

    단속카메라>>카메라 범위 변수명 변경

--- programmers/탐욕법/단속카메라/camera_jiwon.cpp
+++ programmers/탐욕법/단속카메라/camera_jiwon.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int solution(vector<vector<int>> routes) {
     int answer = 1;   
     sort(routes.begin(), routes.end());
-    int advance = routes[0][1]; 
+    int range = routes[0][1]; 
     
     for(int i=1;i<routes.size();i++){
-        if(advance < routes[i][0]){
+        if(range < routes[i][0]){
             answer++; 
-            advance = routes[i][1];
+            range = routes[i][1];
         }
-        if(advance >= routes[i][1]){
-            advance = routes[i][1];
+        if(range >= routes[i][1]){
+            range = routes[i][1];
         }
     }
     return answer;
