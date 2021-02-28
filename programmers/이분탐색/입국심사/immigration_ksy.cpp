ad89f742b7d6c5b67672fdf21be1ba866bf7b8c1
seungyun807 <calebads@gmail.com>
2021-02-28 15:12:09 +0900

    [승윤]순위

--- programmers/이분탐색/입국심사/immigration_ksy.cpp
+++ programmers/이분탐색/입국심사/immigration_ksy.cpp
@@ -7,11 +7,10 @@ using namespace std;
 long long solution(int n, vector<int> times) {
     long long answer = 0;
     sort(times.begin(), times.end());
-    long long itmesize = times.size();
-    long long end = times.back() * n;
-    long long start = (n * times.front()) / itmesize;
-    long long mid = 0;
-    long long num = 0;
+    unsigned long long itmesize = times.size();
+    unsigned long long back = times.back();
+    unsigned long long mid = 0, num = 0, start = 1, m = n, end = back*m;
+    
     while (start <= end) {
         num = 0;
         mid = (end + start) / 2;
@@ -27,5 +26,6 @@ long long solution(int n, vector<int> times) {
                 answer = mid;
             }
     }
+
     return answer;
 }
