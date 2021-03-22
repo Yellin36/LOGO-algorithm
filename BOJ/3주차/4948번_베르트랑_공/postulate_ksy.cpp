9d89dbd404da2ddf01ce1fa1afb00a31d517f1c2
seungyun807 <76242399+seungyun807@users.noreply.github.com>
2021-03-22 19:42:56 +0900

    [승윤]베르트랑공준

--- BOJ/3주차/4948번_베르트랑_공/postulate_ksy.cpp
+++ BOJ/3주차/4948번_베르트랑_공/postulate_ksy.cpp
@@ -6,34 +6,32 @@
 using namespace std;
 
 void solution(int n) {
-    bool* answer;
-    int m = n * 2, j = 1, x = sqrt(m);
-    answer = new bool[m+1];
-    
-    for (int i = 1; i <= m; i++) {
-        answer[i] = 1;
-    }
-    for (int i = 2; i <= x; i++) {
-        while(j*i <= m){
-            answer[j * i] = 0;
-            j++;
-        }
-        j = 1;
-    }
-    j = 0;
-    for (int i = n+1; i <= m; i++){
-        if (answer[i] == 1) {
-            j++;
-        }
-    }
-    cout << j << endl;
+	int m = n * 2, j = 1;
+	vector<bool> answer(m + 1, true);
+
+	for (int i = 2; i <= (int)sqrt(m); i++) {
+		if(answer[i] == true){
+		j = 2;
+		while(j*i <= m){
+				answer[j * i] = 0;
+				j++;
+			}	
+		}
+	}
+	j = 0;
+	for (int i = n+1; i <= m; i++){
+		if (answer[i] == 1) {
+			j++;
+		}
+	}
+	cout << j << endl;
 }
 
 int main() {
     int T = 1;
-    while (T >= 1){
-        cin >> T;
-        if (T != 0) solution(T);
-    }
-    return 0;
+	while (T >= 1){
+		cin >> T;
+		if (T != 0) solution(T);
+	}
+	return 0;
 }
